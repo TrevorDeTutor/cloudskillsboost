5d2c0da0ec74724d2752856a7c93f250358341c7
TrevorNathan <trevoratwijukire@gmail.com>
2023-09-25 20:01:06 +0300

    UPDATE: add network-tag to vm

--- qt-cloud-architecture/cl-01/solution.sh
+++ qt-cloud-architecture/cl-01/solution.sh
@@ -6,8 +6,13 @@ gcloud compute instances create apache2-server \
     --scopes=storage-ro \
     --metadata=startup-script-url=gs://qwiklabs-gcp-03-6b5ada050ff6-bucket/resources-install-web.sh
 
+# add network tag
+gcloud compute instances add-tags apache2-server \
+    --zone=us-east1-d  \
+    --tags=apache2-server
+
 # add firewalls
-gcloud compute firewall-rules create http-allow --allow tcp:80
+gcloud compute firewall-rules create http-allow --allow tcp:80 --target-tags=apache2-server
 
 # access vm
 http://[EXTERNAL_IP]
