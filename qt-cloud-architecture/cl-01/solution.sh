6c66fff62f035e1a73fa301059a7d0e5c334d110
TrevorNathan <trevoratwijukire@gmail.com>
2023-09-25 19:50:21 +0300

    ADD: challenge lab 01 solution

---
+++ qt-cloud-architecture/cl-01/solution.sh
@@ -0,0 +1,13 @@
+#Create a vm passing a startup script stored in Cloud storage
+gcloud compute instances create apache2-server \
+    --zone=us-east1-d \
+    --image-project=debian-cloud \
+    --image-family=debian-10 \
+    --scopes=storage-ro \
+    --metadata=startup-script-url=gs://qwiklabs-gcp-03-6b5ada050ff6-bucket/resources-install-web.sh
+
+# add firewalls
+gcloud compute firewall-rules create http-allow --allow tcp:80
+
+# access vm
+http://[EXTERNAL_IP]
